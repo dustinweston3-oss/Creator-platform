00a8e659e6f8d4b0220bbf7508f6ed7542d310a9
dustinweston3-oss <dustinweston3@gmail.com>
2026-04-02 04:55:00 -0700

    Create app/Main.py

---
+++ App/Databases.py
@@ -0,0 +1,16 @@
+import os
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
